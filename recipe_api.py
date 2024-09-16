aec701f5835807c05828cbbbc59346cb38b18eb9
dairi256 <72542198+dairi256@users.noreply.github.com>
2024-09-16 14:07:34 +0800

    Added two functions into recipe_api. get_random_recipe, which fetches a random recipe from the API and returns a dict that contains the recipe data. find_recipies_by_ingredients finds recipies by a list of ingredients, and returns a list of recipies that match the ingredients.

--- recipe_api.py
+++ recipe_api.py
@@ -1,1 +1,28 @@
-# API key: f0535df3c4fe46ea90194ce80bb6ebb4
+import requests
+import random
+
+API_KEY = 'f0535df3c4fe46ea90194ce80bb6ebb4'
+BASE_URL = 'https://api.spoonacular.com/recipies'
+
+def get_random_recipe():
+    url = f"{BASE_URL}/random?apiKey={API_KEY}"
+    response = requests.get(url)
+
+    # This checks if the request was successful.
+    if response.status_code == 200:
+        recipe = response.json()
+        return recipe['recipies'][0] #This returns the first recipe from the response.
+    else:
+        print("Error fetching random recipe:", response.status_code)
+        return None
+
+def find_recipies_by_ingredients(ingredients):
+    ingredients_str = ','.join(ingredients)
+    url = f"{BASE_URL}/findByIngredients?apiKey={API_KEY}&ingredients={ingredients_str}"
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        return response.json() # Returns the list of matched recipies based on the users prompt. 
+    else:
+        print("Error fetching recipies by ingredients:", response.status_code)
+        return None
